c8a7abe0301a23ddf7032eeabec6075939ad0787
Trevor I <handsome.latino@protonmail.com>
2022-05-06 08:55:25 +0800

    fix donate link

--- src/routes/SupportMe.js
+++ src/routes/SupportMe.js
@@ -9,7 +9,7 @@ export default function SupportMe() {
       <div className='content'>
         <p>Your support can help me create more projects that bring awareness to the strengths of Bitcoin amidst a noisy and distracted world.</p>
         <p>My goal is to add a small contribution to a world where my children, our children can see a brighter future using Bitcoin as a censorship-resistant and decentralized type of money.</p>
-        <p className='donate-button'><LinkButton to="https://pay.handsomelatino.com/invoice?id=T95yLnYYGCbPLGJCdNutip">Donate Bitcoin</LinkButton></p>
+        <p className='donate-button'><LinkButton to="https://pay.handsomelatino.com/api/v1/invoices?storeId=3zecRWVXGTMtpsE4157U8hjzL9iwBfPFBroABh3oWD3r&currency=USD">Donate Bitcoin</LinkButton></p>
       </div>
     </div>
   )
