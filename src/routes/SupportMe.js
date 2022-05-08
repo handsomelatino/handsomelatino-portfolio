96f14e6d6ad17463b89bcb6de32abfbc4adf6b51
Trevor I <handsome.latino@protonmail.com>
2022-05-08 15:55:08 +0800

    add stackernews lightning address

--- src/routes/SupportMe.js
+++ src/routes/SupportMe.js
@@ -10,6 +10,8 @@ export default function SupportMe() {
         <p>Your support can help me create more projects that bring awareness to the strengths of Bitcoin amidst a noisy and distracted world.</p>
         <p>My goal is to add a small contribution to a world where my children, our children can see a brighter future using Bitcoin as a censorship-resistant and decentralized type of money.</p>
         <p className='donate-button'><LinkButton to="https://pay.handsomelatino.com/api/v1/invoices?storeId=3zecRWVXGTMtpsE4157U8hjzL9iwBfPFBroABh3oWD3r&currency=USD">Donate Bitcoin</LinkButton></p>
+        <p>Tipping with lightning? Use Lightning Address: <code>handsome_latino@stacker.news</code></p>
+        <p className='lnurl-button'><LinkButton to='lightning:lnurl1dp68gurn8ghj7um5v93kketj9ehx2amn9uh8wetvdskkkmn0wahz7mrww4excup0dpskuerndakk2hmvv96xjmn02f3vg9'>⚡️ handsome_latino@stacker.news</LinkButton></p>
       </div>
     </div>
   )
